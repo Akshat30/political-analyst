4366deea1a9212911aa887b7355a9a8d28b168b8
Akshat30 <akshat30@gmail.com>
2023-08-26 22:38:51 -0700

    testing genprompt2

--- gpt.py
+++ gpt.py
@@ -1,3 +1,4 @@
+from nltk.tokenize import sent_tokenize
 import requests
 import os
 import openai
@@ -8,6 +9,9 @@ from dotenv import load_dotenv
 import csv
 from scipy.optimize import curve_fit
 import numpy as np
+import nltk
+nltk.download('punkt')
+
 
 class BiasDetector:
 
@@ -52,38 +56,15 @@ class BiasDetector:
         }
 
         response = requests.post(api_url, json=payload, headers=headers)
-        if not response.json()['ok']:   
+        if not response.json()['ok']:
             raise ValueError(response.json()['msg'])
-        
+
         summarized_transcript = response.json()['summary']
 
         return summarized_transcript
-    
+
     def genPrompt(self, text, debug=False):
-        MAX_TOKENS = 4096
-        criteria_num_tokens = self.numTokensFromString(self.criteria) 
-
-        # Calculate the max amt of tokens we can input into prompt after criteria
-        max_text_tokens = MAX_TOKENS - criteria_num_tokens 
-
-        # Current tokens the original text holds
-        curr_text_tokens = self.numTokensFromString(text)
-
-        print("Current amount of total tokens: {0}".format(criteria_num_tokens + curr_text_tokens))
-
-        # Summarize text by 20% each time until it fits into GPT
-        percent = 80
-        summarized = text
-        while percent > 0 and curr_text_tokens > max_text_tokens:
-            summarized = self.summarize(text, percent)
-            curr_text_tokens = self.numTokensFromString(summarized)
-            percent -= 10
-            
-        print("Final amount of total tokens: {0}".format(criteria_num_tokens + curr_text_tokens))
-        return self.criteria + summarized
-    
-    def genPrompt2(self, text, debug=False):
-        MAX_TOKENS = 4096
+        MAX_TOKENS = 3500
         criteria_num_tokens = self.numTokensFromString(self.criteria)
 
         # Calculate the max amt of tokens we can input into prompt after criteria
@@ -96,52 +77,113 @@ class BiasDetector:
             criteria_num_tokens + curr_text_tokens))
 
         # Summarize text by 20% each time until it fits into GPT
-        maxLength = max_text_tokens*5
+        percent = 80
+        summarized = text
+        while percent > 0 and curr_text_tokens > max_text_tokens:
+            summarized = self.summarize(text, percent)
+            curr_text_tokens = self.numTokensFromString(summarized)
+            percent -= 10
+
+        print("Final amount of total tokens: {0}".format(
+            criteria_num_tokens + curr_text_tokens))
+        return self.criteria + summarized
+
+    def genPrompt2(self, text, debug=False):
+        MAX_TOKENS = 3500
+        criteria_num_tokens = self.numTokensFromString(self.criteria)
+
+        # Calculate the max amt of tokens we can input into prompt after criteria
+        max_text_tokens = MAX_TOKENS - criteria_num_tokens
+
+        # Current tokens the original text holds
+        curr_text_tokens = self.numTokensFromString(text)
+
+        print("Char length: " + str(len(text)) + "\n")
+        print("Current amount of total tokens: {0}".format(
+            criteria_num_tokens + curr_text_tokens))
+
+        # Summarize text by 20% each time until it fits into GPT
+        maxLength = max_text_tokens*4.8
 
         if(curr_text_tokens <= max_text_tokens):
             return self.criteria + text
-        
-        # Given data
-        original_lengths = np.array(
-            [25328, 25328, 25328, 25328, 25328, 25328, 25328, 25328, 25328, 25328])
-        returned_lengths = np.array(
-            [25328, 25229, 24545, 23003, 21016, 18147, 15952, 11812, 8273, 4634])
-        summary_percents = np.array([100, 90, 80, 70, 60, 50, 40, 30, 20, 10])
 
-        def power_law(x, k):
-            return original_lengths * (x / 100) ** k
-        
-        params, _ = curve_fit(power_law, summary_percents, returned_lengths)
-        k = params[0]
-        
-        def calculate_summary_percent(desired_length):
-            return (desired_length / original_lengths[0]) ** (1 / k) * 100
+        numSen = len(sent_tokenize(text))
 
-        summary_percent = calculate_summary_percent(maxLength)
-        print(f"Using Summary Percent: {summary_percent:.2f}%")
+        fivePer = self.summarize(text, 4)
+
+        summary_percent = (
+            maxLength/(len(fivePer)/len(sent_tokenize(fivePer))))/numSen * 100
+
+        print("Max Length: " + str(maxLength) + "\n")
+        print("Average sentence length: " +
+              str(len(sent_tokenize(fivePer))) + "\n")
+        print("Num Sentence length: " + str(numSen) + "\n")
+        print("Max sentences: " + str((maxLength/len(sent_tokenize(fivePer)))) + "\n")
+        print("Summary percent: " + str(summary_percent) + "\n")
 
         summarized = self.summarize(text, summary_percent)
 
         curr_text_tokens = self.numTokensFromString(summarized)
         print("Final amount of total tokens: {0}".format(
             criteria_num_tokens + curr_text_tokens))
-        
+
         return self.criteria + summarized
-    
+
     def send(self, prompt, debug=False):
         # Send prompt to chatgpt and display response
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}], temperature = 0)
-        
+            model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}], temperature=0)
+
         if debug:
             print(response)
 
         return response['choices'][0]['message']['content']
     
+    def testGenPrompt2(self, category_link):
+        ex = Extractor()
+        links = ex.getLinks(category_link)
+        for link in links:
+            print('Currently opening article at: ' + link + '\n')
+            text = self.getTranscript(link)
+
+            MAX_TOKENS = 3500
+            criteria_num_tokens = self.numTokensFromString(self.criteria)
+
+            # Calculate the max amt of tokens we can input into prompt after criteria
+            max_text_tokens = MAX_TOKENS - criteria_num_tokens
+
+            # Current tokens the original text holds
+            curr_text_tokens = self.numTokensFromString(text)
+            
+            startTokens = criteria_num_tokens + curr_text_tokens
+
+            # Summarize text by 20% each time until it fits into GPT
+            maxLength = max_text_tokens*4.9
+            reducedTokens = 0
+
+            if(curr_text_tokens > max_text_tokens):
+                numSen = len(sent_tokenize(text))
+
+                fivePer = self.summarize(text, 2)
+
+                summary_percent = (maxLength/(len(fivePer)/len(sent_tokenize(fivePer))))/numSen * 100
+
+                summarized = self.summarize(text, summary_percent)
+
+                curr_text_tokens = self.numTokensFromString(summarized)
+                
+                reducedTokens = curr_text_tokens+criteria_num_tokens
+
+            print("Max Tokens: {0}, Start Tokens: {1}, Reduced Tokens: {2}, Max - Reduced: {3}".format(MAX_TOKENS, startTokens, reducedTokens, MAX_TOKENS-reducedTokens))
+
+
+
     def summarizeFunctionData(self, category_link):
         ex = Extractor()
         links = ex.getLinks(category_link)
-        data = [['Link', 'Summary Percent', 'OG TS Length', 'Summ. TS Length', 'Length Ratio']]
+        data = [['Link', 'Summary Percent', 'OG TS Length',
+                 'Summ. TS Length', 'Length Ratio', 'Sentence Length']]
         for link in links:
             print('Currently opening article at: ' + link + '\n')
             tscript = self.getTranscript(link)
@@ -157,16 +199,17 @@ class BiasDetector:
                 dataRow.append(oglength)
                 dataRow.append(sumlength)
                 dataRow.append(sumlength/oglength)
+                dataRow.append(len(sent_tokenize(summarizedTscript)))
                 data.append(dataRow)
-        
+
         # File path
-        csv_file_path = 'summary_function_data.csv'
+        csv_file_path = 'sentence_data.csv'
 
         # Writing to CSV file
         with open(csv_file_path, mode='w', newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerows(data)
- 
+
     # def compareSummaries(self, summarized_transcripts):
     #     count = 100
     #     for tran in summarized_transcripts:
@@ -191,34 +234,37 @@ class BiasDetector:
     #         charcount = str(len(tran))
     #         ratio = str(len(tran)/len(summarized_transcripts[0]))
     #         gptresponse = response['choices'][0]['message']['content']
-            
+
     #         with open(filetowrite, 'a') as fp:
     #             fp.write("Transcript Percentage {0} and Char Count: {1} and Length ratio: {2} \n\nTranscript: \n{3}\n\nResponse: \n{4}\n \n \n".format(sumpercent, charcount, ratio, tran, gptresponse))
-            
+
     #         count-=20
-    
+
     # def summarizeMultiple(self, text, percents):
     #     summarized_texts = []
     #     for percent in percents:
     #         summarized_texts.append(self.summarize(text, percent))
 
     #     return summarized_texts
-        
+
+
 if __name__ == "__main__":
     print("Enter the link to a transcript/article:")
     url = input()
-    detector = BiasDetector()    
+    detector = BiasDetector()
     transcript = detector.getTranscript(url)
 
     prompt = detector.genPrompt(transcript)
-    print("Number of Tokens Calculated: {0}".format(detector.numTokensFromString(prompt)))
+    print("Number of Tokens Calculated: {0}".format(
+        detector.numTokensFromString(prompt)))
     print(detector.send(prompt))
 
     # Generating summary data
     # print("Enter the link to a rev transcript category page:")
     # url = input()
     # detector = BiasDetector()
-    # detector.summarizeFunctionData(url)
+    # detector.testGenPrompt2(url)
+    # # detector.summarizeFunctionData(url)
 
     # summarized_texts = detector.summarizeMultiple(transcript, [80, 60, 40, 20])
     # detector.testPrompt(summarized_texts)
