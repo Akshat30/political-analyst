c15b8a1defd5a265731f31bce410bdd137d4410c
meganleongg <98578642+meganleongg@users.noreply.github.com>
2023-08-28 16:31:13 -0700

    flexbox nav bar

--- web-app/app/components/Navbar.js
+++ web-app/app/components/Navbar.js
@@ -6,17 +6,31 @@ function Navbar() {
     <nav class="navbar" className='h-10'>
 
         {/* LOGO & PRODUCT NAME IMAGE LINKS  TODO: change later */}
-        <Link href="/" style={{ backgroundColor: '#A9A9A9', marginLeft: '2rem', marginRight: '0.5rem'}}>   </Link>
-        <Link href="/" style={{ backgroundColor: '#A9A9A9', marginRight: '3rem'}}></Link>
 
-        <Link href="/library" className="text-black text-[15px] font-medium leading-normal " style={{ marginRight: '3rem'}}>Library</Link>
-        <Link href="/presidential-candidates" className="text-black text-[15px] font-medium leading-normal" style={{ marginRight: '3rem'}}>Presidential Candidates</Link>
-        <Link href="/about" className="text-black text-[15px] font-medium leading-normal" style={{ marginRight: '3rem'}}>About</Link>
-        <Link href="/register-to-vote" className="text-black text-[15px] font-medium leading-normal" style={{ marginRight: '40rem'}}>Register to Vote</Link>
-
-            <div className="w-32 h-8 px-4 bg-neutral-600 rounded-2xl items-center inline-flex justify-center">
-                <div className="text-zinc-300 text-base font-medium leading-normal">Try Name.Ai </div>
+        <div className="flex items-center">
+            <div className="w-1/6 flex justify-start">
+              <Link href="/" >   </Link>
+              <Link href="/" ></Link>
             </div>
+            <div className="w-1/6 flex justify-start">
+              <Link href="/library" className="text-black text-[15px] font-medium leading-normal " >Library</Link>
+            </div>
+            <div className="w-2/6 flex justify-start">
+              <Link href="/presidential-candidates" className="text-black text-[15px] font-medium leading-normal" >Presidential Candidates</Link>
+            </div>
+            <div className="w-1/6 flex justify-start" style={{marginLeft: '-1rem'}}>
+              <Link href="/about" className="text-black text-[15px] font-medium leading-normal" >About</Link>
+            </div>
+            <div className="w-1/6 flex justify-start">
+              <Link href="https://vote.gov/" className="text-black text-[15px] font-medium leading-normal" >Register to Vote</Link>
+            </div>
+            <div className="w-3/5 flex justify-end" style={{marginRight: '3rem'}}>
+              <div className="w-40 h-8 px-4 bg-neutral-600 rounded-2xl items-center inline-flex justify-center">
+                <div className="text-zinc-300 text-base font-medium leading-normal">Try Name.Ai </div>
+              </div>
+            </div>
+          
+        </div>
         
 
     </nav>
