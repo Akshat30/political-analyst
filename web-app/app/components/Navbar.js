76e22175df5bf4ad0d7127de42dabd5e3cd24828
meganleongg <98578642+meganleongg@users.noreply.github.com>
2023-09-05 15:42:33 -0700

    creates the chart by intervals and counts the recent visits

--- web-app/app/components/Navbar.js
+++ web-app/app/components/Navbar.js
@@ -24,11 +24,14 @@ function Navbar() {
             <div className="w-1/6 flex justify-start">
               <Link href="https://vote.gov/" className="text-black text-[15px] font-medium leading-normal" >Register to Vote</Link>
             </div>
-            <div className="w-3/5 flex justify-end" style={{marginRight: '3rem'}}>
+            <Link href="https://vote.gov/">
+              <div className="w-3/5 flex justify-end" style={{marginRight: '3rem'}}>
               <div className="w-40 h-8 px-4 bg-neutral-600 rounded-2xl items-center inline-flex justify-center">
                 <div className="text-zinc-300 text-base font-medium leading-normal">Try Name.Ai </div>
               </div>
-            </div>
+              </div>
+            </Link>
+            
           
         </div>
         
