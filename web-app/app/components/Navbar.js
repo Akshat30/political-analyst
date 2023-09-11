8625350d87774e4ee552fbd54ecf226226dbadce
meganleongg <98578642+meganleongg@users.noreply.github.com>
2023-09-10 17:22:27 -0700

    link on vernum button

--- web-app/app/components/Navbar.js
+++ web-app/app/components/Navbar.js
@@ -3,42 +3,31 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav class="navbar" className='h-10'>
-
-        {/* LOGO & PRODUCT NAME IMAGE LINKS  TODO: change later */}
-
+    <nav className="navbar h-10">
         <div className="flex items-center">
             <div className="w-1/6 flex justify-start">
-              <Link href="/" >   </Link>
-              <Link href="/" ></Link>
+              <Link href="/">
+                <a>Home</a>  {/* Updated to include an anchor tag */}
+              </Link>
             </div>
             <div className="w-1/6 flex justify-start">
-              <Link href="/library" className="text-black text-[15px] font-medium leading-normal " >Library</Link>
-            </div>
-            <div className="w-2/6 flex justify-start">
-              <Link href="/presidential-candidates" className="text-black text-[15px] font-medium leading-normal" >Presidential Candidates</Link>
-            </div>
-            <div className="w-1/6 flex justify-start" style={{marginLeft: '-1rem'}}>
-              <Link href="/about" className="text-black text-[15px] font-medium leading-normal" >About</Link>
+              <Link href="/library">
+                <a className="text-black text-[15px] font-medium leading-normal">Library</a>
+              </Link>
             </div>
             <div className="w-1/6 flex justify-start">
-              <Link href="https://vote.gov/" className="text-black text-[15px] font-medium leading-normal" >Register to Vote</Link>
+              <a href="https://vote.gov/" className="text-black text-[15px] font-medium leading-normal">Register to Vote</a>
             </div>
-            <Link href="https://vote.gov/">
-              <div className="w-3/5 flex justify-end" style={{marginRight: '3rem'}}>
+            <div className="w-3/5 flex justify-end" style={{marginRight: '3rem'}}>
               <div className="w-40 h-8 px-4 bg-neutral-600 rounded-2xl items-center inline-flex justify-center">
-                <div className="text-zinc-300 text-base font-medium leading-normal">Try Name.Ai </div>
+                <Link href="/vernum">
+                  <a className="text-zinc-300 text-base font-medium leading-normal">Try Vernum</a>
+                </Link>
               </div>
-              </div>
-            </Link>
-            
-          
+            </div>
         </div>
-        
-
     </nav>
   );
 }
 
 export default Navbar;
-
