9773fe1e3ad69a689f8832195b2f471e1a73597e
meganleongg <98578642+meganleongg@users.noreply.github.com>
2023-09-10 18:40:59 -0700

    nav bar

--- web-app/app/components/Navbar.js
+++ web-app/app/components/Navbar.js
@@ -3,34 +3,36 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav className="navbar h-10">
+    <nav class="navbar" className='h-10'>
+
+        {/* LOGO & PRODUCT NAME IMAGE LINKS  TODO: change later */}
+
         <div className="flex items-center">
             <div className="w-1/6 flex justify-start">
-              <Link href="/library">
-                <a className="text-black text-[15px] font-medium leading-normal">Library</a>
-              </Link>
             </div>
             <div className="w-1/6 flex justify-start">
-              <Link href="/presidential-candidates">
-                <a className="text-black text-[15px] font-medium leading-normal">Presidential Candidates</a>
-              </Link>
+              <Link href="/library" className="text-black text-[15px] font-medium leading-normal " >Library</Link>
             </div>
-            <div className="w-1/6 flex justify-start">
-              <Link href="/about">
-                <a className="text-black text-[15px] font-medium leading-normal">About</a>
-              </Link>
+            <div className="w-1/5 flex justify-start">
+              <Link href="/presidential-candidates" className="text-black text-[15px] font-medium leading-normal" >Presidential Candidates</Link>
             </div>
-            <div className="w-1/6 flex justify-start">
-              <a href="https://vote.gov/" className="text-black text-[15px] font-medium leading-normal">Register to Vote</a>
+            <div className="w-1/6 flex justify-center">
+              <Link href="/about" className="text-black text-[15px] font-medium leading-normal" >About</Link>
             </div>
-            <div className="w-3/5 flex justify-end" style={{marginRight: '3rem'}}>
+            <div className="w-1/6 flex justify-center">
+              <Link href="https://vote.gov/" className="text-black text-[15px] font-medium leading-normal" >Register to Vote</Link>
+            </div>
+
+            <Link href="/try-vernum" className="w-2/3">
+              <div className="flex justify-end" style={{marginRight: '3rem'}}>
               <div className="w-40 h-8 px-4 bg-neutral-600 rounded-2xl items-center inline-flex justify-center">
-                <Link href="/vernum">
-                  <a className="text-zinc-300 text-base font-medium leading-normal">Try Vernum</a>
-                </Link>
+                <div className="text-zinc-300 text-base font-medium leading-normal">Try Vernum </div>
               </div>
-            </div>
+              </div>
+            </Link>
         </div>
+        
+
     </nav>
   );
 }
---
+++ web-app/app/try-vernum/page.js
@@ -0,0 +1,11 @@
+// pages/try-vernum.js
+function TryVernum() {
+    return (
+      <div>
+        <h1>Try Vernum Page</h1>
+      </div>
+    );
+  }
+  
+  export default TryVernum;
+  
