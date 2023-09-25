1c623558a767b639385ed6bcaa752651c899010b
Marco Paredes <marco.paredes2001@gmail.com>
2023-09-25 16:46:15 -0700

    Created sendLink and sendText to send request to AWS

---
+++ web-app/app/input-screen/analysis.js
@@ -0,0 +1,81 @@
+/**
+ * Send request with text in body to AWS API Gateway
+ * @param {string} text - text to be sent to AWS API Gateway
+ * @returns {Promise} - Promise object represents response from AWS API Gateway to display to user
+ * @throws {Error} - if text is not a string or is empty
+ * @throws {Error} - if response from AWS API Gateway is not 200
+ */
+export async function sendText(text) {
+    if (typeof text !== 'string' || text === '') {
+        throw new Error('text must be a non-empty string');
+    }
+
+    const url = 'https://6nmdei0ejb.execute-api.us-west-1.amazonaws.com/chat';
+    const body = {
+        text: text,
+        link: ""
+    };
+    const params = {
+        method: 'POST',
+        body: JSON.stringify(body)
+    };
+
+    return await fetch(url, params)
+        .then((response) => {
+            if (response.status !== 200) {
+                throw new Error(`Error ${response.status}: ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data)
+            return data;
+        })
+        .catch((err) => {
+            throw err;
+        });
+    
+}
+
+/**
+ * Send request with link in body to AWS API Gateway
+ * @param {string} link - link to be sent to AWS API Gateway
+ * @returns {Promise} - Promise object represents response from AWS API Gateway to display to user
+ * @throws {Error} - if link is not a string or is empty
+ * @throws {Error} - if response from AWS API Gateway is not 200
+ */
+export async function sendLink(link) {
+    if (typeof link !== 'string' || link === '') {
+        throw new Error('link must be a non-empty string');
+    }
+
+    const url = 'https://6nmdei0ejb.execute-api.us-west-1.amazonaws.com/chat';
+    const body = {
+        text: "",
+        link: link
+    };
+
+    const params = {
+        mode: 'no-cors',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    };
+
+    return await fetch(url, params)
+        .then((response) => {
+            if (response.status !== 200) {
+                throw new Error(`${response.status}: ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log(`Response from AWS: ${data}`)
+            return data;
+        })
+        .catch((err) => {
+            throw err;
+        });
+}
---
+++ web-app/app/input-screen/page.js
@@ -0,0 +1,27 @@
+"use client";
+
+import '../upload-text/upload-text-styles.css';
+import React, { useState } from 'react';
+import Image from 'next/image'
+import Link from 'next/link';
+import { sendText, sendLink } from './analysis.js';
+
+export default function InputScreen() {
+    return (
+      <>
+        <div className="w-full h-full py-3 flex flex-col">
+          <div className="ml-20">
+            <h1 className="text-[#131313] text-4xl font-bold">
+              Test
+            </h1>
+            <div className="w-2/3 mt-4">
+              <p className="text-gray-400 text-lg font-semibold">
+                {sendLink("https://www.cnn.com/2023/09/15/opinions/mitt-romney-republicans-senate-retirement-trump-chen/index.html")}
+              </p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+
+  }
