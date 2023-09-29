97b022f999b7c3110fa55c1a2189d4c6bff348c7
Marco Paredes <marco.paredes2001@gmail.com>
2023-09-29 14:46:22 -0700

    analysis functions

--- web-app/app/analysis/analysis.js
+++ web-app/app/analysis/analysis.js
@@ -11,17 +11,18 @@ export async function sendText(text) {
         throw new Error('text must be a non-empty string');
     }
 
-    const url = 'https://6nmdei0ejb.execute-api.us-west-1.amazonaws.com/chat';
+    const url = 'https://xhjwcepvug.execute-api.us-west-1.amazonaws.com/analysis';
     const body = {
         text: text,
         link: ""
     };
     const params = {
+        mode: 'no-cors',
         method: 'POST',
         body: JSON.stringify(body)
     };
 
-    return await fetch(url, params)
+    return fetch(url, params)
         .then((response) => {
             if (response.status !== 200) {
                 throw new Error(`Error ${response.status}: ${response.statusText}`);
@@ -51,7 +52,7 @@ export async function sendLink(link) {
         throw new Error('link must be a non-empty string');
     }
 
-    const url = 'https://6nmdei0ejb.execute-api.us-west-1.amazonaws.com/chat';
+    const url = 'https://xhjwcepvug.execute-api.us-west-1.amazonaws.com/analysis';
     const body = {
         text: "",
         link: link
@@ -66,7 +67,7 @@ export async function sendLink(link) {
         body: JSON.stringify(body)
     };
 
-    return await fetch(url, params)
+    return fetch(url, params)
         .then((response) => {
             if (response.status !== 200) {
                 throw new Error(`${response.status}: ${response.statusText}`);
@@ -74,7 +75,7 @@ export async function sendLink(link) {
             return response.json();
         })
         .then((data) => {
-            console.log(`Response from AWS: ${data}`)
+            console.log(`Response from AWS: \n${data}`)
             return data;
         })
         .catch((err) => {
